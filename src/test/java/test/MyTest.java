6b28a4203c22bf106ad25d6ce178fa162679113a
caidifa <caidifa@126.com>
2017-02-03 21:08:23 +0800

    增加了Mytest

---
+++ src/test/java/test/MyTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import org.junit.Test;
+
+/**
+ * Created by caibaolong on 2017/1/12.
+ * 基本测试
+ */
+public class MyTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+
+    @Test
+    public void test3() {
+
+
+    }
+
+    @Test
+    public void test4() {
+
+    }
+
+
+}
